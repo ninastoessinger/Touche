610bdc08d362c6944d114d45c3424c08d45e5578
ninastoessinger <altairas_eye@mac.com>
2014-10-15 16:44:27 +0200

    new way of calculating intersections
    thanks to @typemytype

--- Touche.roboFontExt/lib/Touche.py
+++ Touche.roboFontExt/lib/Touche.py
@@ -3,19 +3,20 @@
 """
  Touché! 
  RoboFont extension that detects and displays glyph pairs whose bodies overlap or touch.
- v1.0 - October 2014
- nina@typologic.nl
+ v1.2 - improved and much faster – profuse thanks to Frederik Berlaen
+ Nina Stössinger, October 2014
 """
 
-from robofab.interface.all.dialogs import PutFile, Message
+import findPossibleOverlappingSegmentsPen
+reload(findPossibleOverlappingSegmentsPen)
+
 from vanilla import CheckBox, Group, List, ProgressSpinner, SquareButton, TextBox, Window
 from mojo.UI import MultiLineView, OpenSpaceCenter
 from mojo.roboFont import OpenWindow
+from fontTools.misc.arrayTools import offsetRect, sectRect
+from lib.tools.bezierTools import intersectCubicCubic, intersectCubicLine, intersectLineLine
+from robofab.interface.all.dialogs import PutFile, Message
 import time
-
-import shiftedDigestPointPen
-import findPointOnSegment
-
 
 class ToucheTool():
     
@@ -77,7 +78,7 @@ class ToucheTool():
     def showPair(self, sender=None):
         try:
             index = sender.getSelection()[0]
-            glyphs = [self.f[gName] for gName in self.touche[index]]
+            glyphs = [self.f[gName] for gName in self.touchingPairs[index]]
             self.w.preview.set(glyphs)
         except IndexError:
             pass
@@ -85,7 +86,7 @@ class ToucheTool():
     def showAllPairs(self, sender=None):
         # open all resulting pairs in Space Center
         rawString = ""
-        for g1, g2 in self.touche:
+        for g1, g2 in self.touchingPairs:
             rawString += "/%s/%s/space" % (g1, g2)
         s = OpenSpaceCenter(self.f)
         s.setRaw(rawString)
@@ -95,29 +96,13 @@ class ToucheTool():
         path = PutFile("Choose save location", "TouchingPairs.txt")
         if path is not None:
             reportString = "#KPL:P: TouchingPairs\n"
-            for g1, g2 in self.touche:
+            for g1, g2 in self.touchingPairs:
                 reportString += "%s %s\n" % (g1, g2)
             fi = open(path,'w+')
             fi.write(reportString)
             fi.close()  
             
     # checking
-    
-    def identifyTouchingPair(self, g1, g2):
-        # only continue if bounding boxes overlap
-        if self._boxOverlap(g1, g2):
-            # so do their bodies overlap?
-            if self._booleanCheck(g1, g2):
-                return True
-            # or do the contours merely touch in some point?
-            elif self._sharesPoints(g1, g2):
-                return True
-        return False
-        
-    def _boxExists(self, g):
-        if g.box is not None:
-            return True
-        return False
         
     def _hasSufficientWidth(self, g):
         # to ignore combining accents and the like
@@ -127,131 +112,10 @@ class ToucheTool():
                 return False
         return True
         
-    def _boxOverlap(self, g1, g2):
-        kern = self._findKern(g1, g2)
-        dist = g1.width + kern
-        self.distanceDict[(g1, g2)] = dist
-        # horizontal overlap
-        if g2.box[0] + dist <= g1.box[2]:
-            if g2.box[2] + dist > g1.box[0]:
-                # vertical overlap
-                if g1.box[3] > g2.box[1] and g2.box[3] > g1.box[1]:
-                    return True
-        return False
-        
-            
-    def _booleanCheck(self, g1, g2):
-        # build shifted glyph
-        tmpShifted = RGlyph()
-        dist = self.distanceDict[(g1, g2)]
-        tmpShifted.appendGlyph(g2.copy(), offset=(dist,0))
-        # make boolean intersection to see if the 2 glyphs share a common area
-        o = tmpShifted & g1
-        if o.isEmpty():
-            # now we know they don't *overlap*, but maybe they just touch along a collinear vector?
-            # make union + compare contour count
-            t = tmpShifted | g1
-            if len(t) < len(g2) + len(g1):
-                return True
-            return False
-        return True
-        
-        
-    def _sharesPoints(self, g1, g2):
-        # this fires if 2 on-curve points from the two glyphs respectively come to lie exactly on top of each other
-        # this makes false positives
-        onCurvePointList = {}
-        allPointList = {}
-        if (g1 != g2): # false positives for successions of the same glyph
-            dist = self.distanceDict[(g1, g2)]
-            #print "dist /%s/%s:" % (g1.name, g2.name), dist
-            pens = {
-                g1: shiftedDigestPointPen.ShiftedDigestPointPen((0,0)),
-                g2: shiftedDigestPointPen.ShiftedDigestPointPen((dist,0))}
-            for g in [g1, g2]:
-                g.drawPoints(pens[g])
-                onCurvePointList[g] = pens[g].getPointDigest()
-                allPointList[g] = pens[g].getPointDigest(onCurveOnly=False)
-            if len(onCurvePointList[g1]) > 0 and len(onCurvePointList[g2]) > 0:
-                for p1 in onCurvePointList[g1]:
-                    for p2 in onCurvePointList[g2]:
-                        if p1[0] == p2[0]:
-                            return True
-                    
-            # Check if an on-curve point touches the other contour in a place where there isn't a point
-            # I'm retiring this for the moment because it makes false positives and does more harm than good
-            """
-            directions = [(g1, g2), (g2, g1)]
-            for item1, item2 in directions:
-                if len(onCurvePointList[item1]) > 0 and len(allPointList[item2]) > 0:
-                    for p1 in onCurvePointList[item1]:
-                        print "checking point at coord", p1[0], "in glyph", item1.name
-                        #p2s = allPointList[item2]
-                        p2s = onCurvePointList[item2]
-                        for i in range(len(p2s)):
-                            found = False
-                            h = i-1 if i > 0 else len(p2s) - 1
-                            if p2s[i][1] == "line":
-                                startPoint = p2s[h][0] 
-                                endPoint   = p2s[i][0] 
-                                found = findPointOnSegment.findPointOnLine(startPoint, endPoint, p1[0])
-                            # and curves? see below *
-                            if found:
-                                #print "found: point at", p1[0], "touches line between", startPoint, "and", endPoint
-                                return True
-            """
-        return False
-        """
-        * This does not check for cases (for now) where a contour outside of a point touches a contour outside of a point, or a corner touches a curve (outside of a point) in the second contour, without producing an overlap which would have been caught earlier. It seems quite unlikely that those would exactly touch in one coordinate (I can’t even come up with a good testing scenario for this). Or should coordinates be rounded, allowing near-misses to qualify too? Not sure.
-        """
-        
-
-        
-    # kerning stuff
-        
-    def _findKern(self, g1, g2):
-        # Find the relevant kerning value for a given pair
-        # glyph - glyph
-        key = (g1.name, g2.name)
-        if self.f.kerning.has_key(key):
-            return self.f.kerning[key]
-        # group - glyph
-        if self.kernGroups.has_key(g1.name):
-            for groups in self.kernGroups[g1.name]:
-                key = (groups, g2.name)
-                if self.f.kerning.has_key(key):
-                    return self.f.kerning[key]
-        # glyph - group
-        if self.kernGroups.has_key(g2.name):
-            for groups in self.kernGroups[g2.name]:
-                key = (g1.name, groups)
-                if self.f.kerning.has_key(key):
-                    return self.f.kerning[key]   
-        # group - group          
-        if self.kernGroups.has_key(g1.name) and self.kernGroups.has_key(g2.name):
-            for groups1 in self.kernGroups[g1.name]:
-                for groups2 in self.kernGroups[g2.name]:
-                    key = (groups1, groups2)
-                    if self.f.kerning.has_key(key):
-                        return self.f.kerning[key]
-        return 0
-        
-    def _buildKernGroupDict(self, glyphList):
-        # Initially collect kern groups for easy referencing by self._findKern
-        kernGroups = {}
-        for g in glyphList:
-            groups = self.f.groups.findGlyph(g.name)
-            if len(groups) > 0:
-                kernGroups[g.name] = groups
-        return kernGroups
-        
-        
-    # some other helper stuff
-        
     def _trimGlyphList(self, glyphList):
         newGlyphList = []
         for g in glyphList:
-            if self._boxExists(g) and self._hasSufficientWidth(g):
+            if g.box is not None and self._hasSufficientWidth(g):
                 newGlyphList.append(g)
         return newGlyphList
         
@@ -260,6 +124,60 @@ class ToucheTool():
         targetWidth = 700 if enlarge else 180
         self.w.setPosSize((posSize[0], posSize[1], targetWidth, posSize[3]))
             
+            
+    # new checking method contributed by Frederik
+
+    def touche(self, g1, g2, kern=0):
+        # get the bounds and check them
+        bounds1 = g1.box
+        if bounds1 is None:
+            return False
+        bounds2 = g2.box
+        if bounds2 is None:
+            return False    
+    
+        # shift bounds2
+        bounds2 = offsetRect(bounds2, g1.width+kern, 0)
+        # check for intersection bounds
+        intersectingBounds, _ = sectRect(bounds1, bounds2)
+        if not intersectingBounds:
+            return False
+        # move bounds1 back, moving bounds is faster then moving all coordinates in a glyph
+        bounds1 = offsetRect(bounds1, -g2.width-kern, 0)
+    
+        # create a pen for g1 with a shifted rect, draw the glyph into the pen
+        pen1 = findPossibleOverlappingSegmentsPen.FindPossibleOverlappingSegmentsPen(g1.getParent(), bounds2)
+        g1.draw(pen1)
+    
+        # create a pen for g2 with a shifted rect and move each found segment with the width and kerning
+        pen2 = findPossibleOverlappingSegmentsPen.FindPossibleOverlappingSegmentsPen(g2.getParent(), bounds1, (g1.width+kern, 0))
+        # draw the glyph into the pen
+        g2.draw(pen2)
+    
+        # loop over all possible overlapping segments
+        for segment1 in pen1.segments:
+            for segment2 in pen2.segments:
+                if len(segment1) == 4 and len(segment2) == 4:
+                    a1, a2, a3, a4 = segment1
+                    b1, b2, b3, b4 = segment2
+                    result = intersectCubicCubic(a1, a2, a3, a4, b1, b2, b3, b4)
+                elif len(segment1) == 4:
+                    p1, p2, p3, p4 = segment1
+                    a1, a2 = segment2
+                    result = intersectCubicLine(p1, p2, p3, p4, a1, a2)
+                elif len(segment2) == 4:
+                    p1, p2, p3, p4 = segment2
+                    a1, a2 = segment1
+                    result = intersectCubicLine(p1, p2, p3, p4, a1, a2)
+                else:
+                    a1, a2 = segment1
+                    b1, b2 = segment2
+                    result = intersectLineLine(a1, a2, b1, b2)
+                if result.status == "Intersection":
+                    return True
+        return False
+    
+
             
     # ok let's do this
     
@@ -271,37 +189,36 @@ class ToucheTool():
             time0 = time.time()
             self.excludeZeroWidth = excludeZeroWidth
             self.f = f 
+            flatKerning = f.naked().flatKerning
     
             glyphNames = f.selection if useSelection else f.keys()
             glyphList = [f[x] for x in glyphNames]
             glyphCount = len(glyphList)
             glyphList = self._trimGlyphList(glyphList)
             
-            self.kernGroups = self._buildKernGroupDict(glyphList)
-            self.distanceDict = {}
-            self.touche = []
+            self.touchingPairs = []
 
             # check pairs
             for g1 in glyphList:
                 for g2 in glyphList:
-                    if self.identifyTouchingPair(g1, g2):
-                        self.touche.append((g1.name, g2.name))
+                    if self.touche(g1, g2, flatKerning.get((g1.name, g2.name), 0)):
+                        self.touchingPairs.append((g1.name, g2.name))
         
             # display output
             self.w.results.stats.set("%d glyphs checked" % glyphCount)
-            self.w.results.result.set("%d touching pairs found" % len(self.touche))
+            self.w.results.result.set("%d touching pairs found" % len(self.touchingPairs))
             self.w.results.show(True)
             
-            outputList = [{"left glyph": g1, "right glyph": g2} for (g1, g2) in self.touche]
+            outputList = [{"left glyph": g1, "right glyph": g2} for (g1, g2) in self.touchingPairs]
             self.w.outputList.set(outputList)
-            if len(self.touche) > 0:
+            if len(self.touchingPairs) > 0:
                 self.w.outputList.setSelection([0])
             else:
                 self.w.preview.set("")
             
             outputButtons = [self.w.results.spaceView, self.w.results.exportTxt]
             for b in outputButtons:
-                b.enable(False) if len(self.touche) == 0 else b.enable(True)
+                b.enable(False) if len(self.touchingPairs) == 0 else b.enable(True)
             self.w.preview.setFont(f)
             self.w.options.progress.stop()
             self._resizeWindow(enlarge=True)
@@ -310,6 +227,6 @@ class ToucheTool():
             print u'Touché: finished checking %d glyphs in %.2f seconds' % (glyphCount, time1-time0)
             
         else:
-            Message(u'Touché: Can’t find a font to check.')
+            Message(u'Touché: Can’t find a font to check')
             
 OpenWindow(ToucheTool)
---
+++ Touche.roboFontExt/lib/findPossibleOverlappingSegmentsPen.py
@@ -0,0 +1,40 @@
+from fontTools.pens.basePen import BasePen
+from fontTools.misc.arrayTools import pointInRect
+
+
+class FindPossibleOverlappingSegmentsPen(BasePen):
+    
+    def __init__(self, glyphSet, bounds, moveSegment=(0, 0)):
+        BasePen.__init__(self, glyphSet)
+
+        self.segments = set()
+        self.bounds = bounds
+        self.moveSegment = moveSegment
+    
+    def addSegment(self, segment):
+        mx, my = self.moveSegment
+        segment = tuple((x+mx, y+my) for x, y in segment)
+        self.segments.add(segment)
+    
+    def _moveTo(self, pt):
+        self.previousPoint = pt
+        self.firstPoint = pt
+    
+    def _lineTo(self, pt):
+        if pointInRect(pt, self.bounds):
+            self.addSegment((self.previousPoint, pt))
+        self.previousPoint = pt
+    
+    def _curveToOne(self, pt1, pt2, pt3):
+        if pointInRect(pt1, self.bounds):
+            self.addSegment((self.previousPoint, pt1, pt2, pt3))
+        elif pointInRect(pt2, self.bounds):
+            self.addSegment((self.previousPoint, pt1, pt2, pt3))
+        elif pointInRect(pt3, self.bounds):
+            self.addSegment((self.previousPoint, pt1, pt2, pt3))
+        self.previousPoint = pt3
+        
+    def closePath(self):
+        if self.firstPoint != self.previousPoint:
+            self.lineTo(self.firstPoint)
+            
